7fd8505cb106a0a81ad4ec244de3abbc5507d1db
handsome745 <handsome745@gmail.com>
2016-06-17 09:07:17 +0300

    qwe

---
+++ AutoSiteProject/AutoSiteProject.Bl/Managers/CarModelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using AutoMapper;
+using AutoSiteProject.Models.DB;
+using AutoSiteProject.Models.ViewModels;
+
+namespace AutoSiteProject.Bl.Managers
+{
+    public class CarModelManager : RepositoryManager<CarModel, CarModelViewModel>
+    {
+        public override CarModelViewModel GetById(int id)
+        {
+            return Mapper.Map<CarModel, CarModelViewModel>(Repository.FindBy(c => c.Id == id).FirstOrDefault());
+        }
+    }
+}
