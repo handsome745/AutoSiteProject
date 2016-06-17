d0a6088400263ba7b7b77278d7d8e354d8d10769
handsome745 <handsome745@gmail.com>
2016-06-17 15:15:09 +0300

    Main page, js for main page

--- AutoSiteProject/AutoSiteProject.UI/Controllers/HomeController.cs
+++ AutoSiteProject/AutoSiteProject.UI/Controllers/HomeController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoSiteProject.Models.Bl.Interfaces;
+using AutoSiteProject.Models.ViewModels;
 
 namespace AutoSiteProject.UI.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        private ICarModelManager _carModelManager;
+        private IManufacturerManager _manufacturerManager;
+        private ICountryManager _countryManager;
+
+        public HomeController(ICarModelManager carModelManager,
+            IManufacturerManager manufacturerManager,
+            ICountryManager countryManager)
         {
-            return RedirectToAction("List", "Country");
+            _carModelManager = carModelManager;
+            _manufacturerManager = manufacturerManager;
+            _countryManager = countryManager;
+        }
+
+        // GET: Home
+        public ActionResult Index(int countryId = -1)
+        {
+            ViewBag.Countries = new SelectList(_countryManager.GetAll(), "Id", "Name");
+            ViewBag.Manufacturers = new SelectList(new List<ManufacturerViewModel>(), "Id", "Name");
+            ViewBag.CarModels = new SelectList(_carModelManager.GetAll(), "Id", "Name");
+            return View();
+        }
+
+        public JsonResult GetManufacturersOfCountry(int id)
+        {
+            var result = _manufacturerManager.GetAll().Where(m => m.CountryId == id);
+            return Json(result);
         }
     }
 }
---
+++ AutoSiteProject/AutoSiteProject.UI/scripts/AutoSite/DataLoader.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $("#country").change(function () {
+        var id = $("#country option:selected").val();
+        DataLoader.loadManufacturers(id);
+    });
+});
+
+DataLoader = {
+    loadManufacturers: function(id) {
+        $.ajax({
+            type: "GET",
+            asynch: true,
+            url: "home/getmanufacturersofcountry/" + id,
+            success: function (output) {
+                DataLoader.renderManufacturersList(output);
+            },
+            error: function (err) {
+                alert("Error");
+            }
+        });
+    },
+    renderManufacturersList: function(data) {
+        var result = "";
+        for (var i = 0; i < data.length; i++) {
+            result += "<option value='" + data[i].Id + "'>";
+            result += data[i].Name;
+            result += "</option>";
+        }
+        document.getElementById("SelectedManufacturer").innerHTML = result;
+    }
+
+}
