11896ab474bc355158f4bac72b62b9dc0997dcbd
handsome745 <handsome745@gmail.com>
2016-06-20 17:36:50 +0300

    CarItem, CarOption database entities. Rewrited sql scripts. Added 2 ViewModels.

---
+++ AutoSiteProject/AutoSiteProject.ViewModels/ViewModels/CarItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSiteProject.Models.ViewModels
+{
+    public class CarItemViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [DisplayName("Car model")]
+        public Nullable<int> ModelId { get; set; }
+        [DisplayName("Body type")]
+        public Nullable<int> BodyTypeId { get; set; }
+
+        public virtual CarBodyTypeViewModel CarBodyType { get; set; }
+        public virtual CarModelViewModel CarModel { get; set; }
+    }
+}
